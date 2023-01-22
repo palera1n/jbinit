ae98b1d06301ad3031c8b7b42972826c9a641be1
Nick Chan <towinchenmi@gmail.com>
2023-01-22 21:34:11 +0800

    fix rootless sbreload

--- src/jbloader.c
+++ src/jbloader.c
@@ -604,12 +604,21 @@ int uicache_loader()
 
 int sbreload()
 {
+  if (checkrain_option_enabled(pinfo.flags, palerain_option_rootful)) {
   if (access("/usr/bin/sbreload", F_OK) != 0)
     return 0;
   char *args[] = {
       "/usr/bin/sbreload",
       NULL};
   return run(args[0], args);
+  } else {
+  if (access("/var/jb/usr/bin/sbreload", F_OK) != 0)
+    return 0;
+  char *args[] = {
+      "/var/jb/usr/bin/sbreload",
+      NULL};
+  return run(args[0], args);
+  }
 }
 
 int jbloader_main(int argc, char **argv)
