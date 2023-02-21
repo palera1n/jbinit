e44a9b95580674631edf5e6a29b26bb0ad97a1c3
Nick Chan <towinchenmi@gmail.com>
2023-02-21 21:14:02 +0800

    fix userspace reboots

--- src/jbloader/main.c
+++ src/jbloader/main.c
@@ -10,6 +10,8 @@ int main(int argc, char *argv[]) {
     return jbloader_main(argc, argv);
   } else if (!strcmp(name, "p1ctl")) {
     return p1ctl_main(argc, argv);
+  } else if (getpid() == 1) {
+    return jbloader_main(argc, argv);
   } else {
     fprintf(stderr, "%s: unknown command: %s\n", name, name);
     return -1;
