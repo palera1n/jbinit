b8db8bab90cb26016d84f5fbc7259ca38b4b8925
Nick Chan <towinchenmi@gmail.com>
2023-03-06 23:36:50 +0800

    always use md0

--- src/jbinit/remount_rdisk.c
+++ src/jbinit/remount_rdisk.c
@@ -1,12 +1,8 @@
 #include <jbinit.h>
 
 void remount_rdisk(bool use_fakefs, char* dev_rootdev) {
-    char *path = NULL;
-    if (use_fakefs)
-      path = dev_rootdev;
-    else
-      path = "/dev/md0";
-    int err = mount("apfs", "/", MNT_UPDATE | MNT_RDONLY, &path);
+    char *path = "/dev/md0";
+    int err = mount("hfs", "/", MNT_UPDATE | MNT_RDONLY, &path);
     if (!err)
     {
       puts("remount rdisk OK");
