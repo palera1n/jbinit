3dccf8ba88cdbf4a634f71c1157178b00413c7da
Tom E <guacaplushy@icloud.com>
2023-04-22 09:38:20 -0400

    remove this useless thing

--- src/launchd_hook/jb.c
+++ src/launchd_hook/jb.c
@@ -129,42 +129,6 @@ bool my_xpc_dictionary_get_bool(xpc_object_t dictionary, const char *key) {
 
 DYLD_INTERPOSE(my_xpc_dictionary_get_bool, xpc_dictionary_get_bool);
 
-/*
-  Every single process is allowed to lookup bootstrap ports starting with "jb-global-"
-  This is inteded to pass tfp0 to sandboxed processes used for debugging kernel stuff with Xcode.
-
-  Additionally, every sandboxed process is allowed to lookup and register bootstrap ports starting with "jb-global-unsandbox-".
-  This can be used for registering bootstrap ports from sandboxed processes and looking them up in other sandboxed processes.
-*/
-int my_sandbox_check_by_audit_token(audit_token_t au, const char *operation, int sandbox_filter_type, ...) {
-    va_list a;
-    va_start(a, sandbox_filter_type);
-    const char *name = va_arg(a, const char *);
-    const void *arg2 = va_arg(a, void *);
-    const void *arg3 = va_arg(a, void *);
-    const void *arg4 = va_arg(a, void *);
-    const void *arg5 = va_arg(a, void *);
-    const void *arg6 = va_arg(a, void *);
-    const void *arg7 = va_arg(a, void *);
-    const void *arg8 = va_arg(a, void *);
-    const void *arg9 = va_arg(a, void *);
-    const void *arg10 = va_arg(a, void *);
-    va_end(a);
-    if (name && operation) {
-        if (strcmp(operation, "mach-lookup") == 0) {
-            if (strncmp((char *)name, "jb-global-", sizeof("jb-global-")-1) == 0) {
-                  return 0;
-            }
-        }else if (strcmp(operation, "mach-register") == 0) {
-            if (strncmp((char *)name, "jb-global-unsandbox-", sizeof("jb-global-unsandbox-")-1) == 0) {
-                return 0;
-            }
-        }
-    }
-    return sandbox_check_by_audit_token(au, operation, sandbox_filter_type, name, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
-}
-DYLD_INTERPOSE(my_sandbox_check_by_audit_token, sandbox_check_by_audit_token);
-
 int _my__NSGetExecutablePath(char* buf, uint32_t* bufsize) {
   if (getpid() != 1) return _NSGetExecutablePath(buf, bufsize);
   else if (*bufsize > sizeof("/cores/jbloader")) {
