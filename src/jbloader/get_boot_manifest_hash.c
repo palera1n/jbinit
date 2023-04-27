870bf8a975c08376efab89e572c276f70e95faa4
Nick Chan <towinchenmi@gmail.com>
2023-04-28 01:27:03 +0800

    fix some more boot manifesh hash memory leaks

--- src/jbloader/get_boot_manifest_hash.c
+++ src/jbloader/get_boot_manifest_hash.c
@@ -5,10 +5,12 @@ int get_boot_manifest_hash(char hash[97])
   const UInt8 *bytes;
   CFIndex length;
   io_registry_entry_t chosen = IORegistryEntryFromPath(0, "IODeviceTree:/chosen");
-  assert(chosen);
+  if (!MACH_PORT_VALID(chosen)) return 1;
   CFDataRef manifestHash = (CFDataRef)IORegistryEntryCreateCFProperty(chosen, CFSTR("boot-manifest-hash"), kCFAllocatorDefault, 0);
+  IOObjectRelease(chosen);
   if (manifestHash == NULL || CFGetTypeID(manifestHash) != CFDataGetTypeID())
   {
+    if (manifestHash != NULL) CFRelease(manifestHash);
     return 1;
   }
   length = CFDataGetLength(manifestHash);
