afd780d2a091aec187ed3a6757b5bdabf3898ca9
Nick Chan <towinchenmi@gmail.com>
2023-05-02 12:54:08 +0800

    make this less terrible

--- src/jbloader/helper/info.c
+++ src/jbloader/helper/info.c
@@ -4,11 +4,11 @@ int get_kflags() {
     struct kerninfo kinfo;
     int ret = get_kerninfo(&kinfo, RAMDISK);
     if (ret != 0) {
-        printf("%s %d\n", "Failed to read kerninfo:", ret);
+        fprintf(stderr, "%s %d\n", "Failed to read kerninfo:", ret);
         return ret;
     }
-    char buf[256];
-    sprintf(buf, "%d\n", kinfo.flags);
+    char buf[16];
+    snprintf(buf, 16, "%d\n", kinfo.flags);
     write(STDOUT_FILENO, &buf, strlen(buf) + 1);
     return 0;
 }
@@ -17,11 +17,11 @@ int get_pflags() {
     struct paleinfo pinfo;
     int ret = get_paleinfo(&pinfo, RAMDISK);
     if (ret != 0) {
-        printf("%s %d\n", "Failed to read paleinfo:", ret);
+        fprintf(stderr, "%s %d\n", "Failed to read paleinfo:", ret);
         return ret;
     }
-    char buf[256];
-    sprintf(buf, "%d\n", pinfo.flags);
+    char buf[16];
+    snprintf(buf, 16, "%d\n", pinfo.flags);
     write(STDOUT_FILENO, &buf, strlen(buf) + 1);
     return 0;
 }
@@ -30,12 +30,10 @@ int get_bmhash() {
     char hash[97];
     int ret = get_boot_manifest_hash(hash);
     if (ret != 0) {
-        printf("%s %d\n", "Failed to get boot manifest hash:", ret);
+        fprintf(stderr, "%s %d\n", "Failed to get boot manifest hash:", ret);
         return ret;
     }
  
-    char buf[256];
-    sprintf(buf, "%s\n", hash);
-    write(STDOUT_FILENO, &buf, strlen(buf) + 1);
+    write(STDOUT_FILENO, &hash, strlen(hash) + 1);
     return 0;
 }
--- src/jbloader/helper/pwset.c
+++ src/jbloader/helper/pwset.c
@@ -2,7 +2,6 @@
 
 int setpw(char *pw) {
     int fd[2];
-    char buf[256];
 
     const char *bin = "/var/jb/usr/sbin/pw"; // todo - prefix
     char *arg[] = {"pw", "usermod", "501", "-h", "0", NULL};
@@ -10,14 +9,17 @@ int setpw(char *pw) {
     if (pipe(fd) == -1) {
         return EPIPE;
     }
-    
-    sprintf(buf, "%s\n", pw);
-    write(fd[1], &buf, strlen(buf) + 1);
 
     dup2(fd[0], STDIN_FILENO);
-    close(fd[0]);
-    close(fd[1]);
 
     pid_t pid;
-    return posix_spawn(&pid, bin, NULL, NULL, arg, NULL);
+    int ret = posix_spawn(&pid, bin, NULL, NULL, arg, NULL);
+    if (ret) {
+        fprintf(stderr, "Could not spawn %s: %d (%s)\n", bin, errno, strerror(errno));
+        return ret;
+    }
+    dprintf(fd[1], "%s\n", pw);
+    close(fd[0]);
+    close(fd[1]);
+    return 0;
 }
