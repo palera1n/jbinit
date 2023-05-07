3cb1537b72227489fdb475c0f8ac80e333641086
Nick Chan <towinchenmi@gmail.com>
2023-05-08 01:48:59 +0800

    use hard tabs in cfprefsd.c

--- src/systemhooks/cfprefsd.c
+++ src/systemhooks/cfprefsd.c
@@ -13,24 +13,24 @@
 void NSLog(CFStringRef format, ...);
 
 bool preferencePlistNeedsRedirection(char* plistPath) {
-    if (
-        strncmp(plistPath, "/private/var/mobile/Containers",sizeof("/private/var/mobile/Containers")-1) == 0 ||
-        strncmp(plistPath, "/var/db",sizeof("/var/db")-1) == 0 ||
-        strncmp(plistPath, "/var/jb",sizeof("/var/jb")-1) == 0
-    ) return false;
-    char plistName[MAXPATHLEN];
-    char* ptr = basename_r(plistPath, plistName);
-    if (ptr == NULL) {
-        NSLog(CFSTR("cfprefsd_hook: basename_r failed: %d (%s)"), errno, strerror(errno));
-        abort();
-    }
-    if (
-        strncmp(plistName, "com.apple.", sizeof("com.apple.")-1) == 0 ||
-        strncmp(plistName, "systemgroup.com.apple.", sizeof("systemgroup.com.apple.")-1) == 0 ||
-        strncmp(plistName, "group.com.apple.", sizeof("group.com.apple.")-1) == 0
-    ) return false;
+	if (
+		strncmp(plistPath, "/private/var/mobile/Containers",sizeof("/private/var/mobile/Containers")-1) == 0 ||
+		strncmp(plistPath, "/var/db",sizeof("/var/db")-1) == 0 ||
+		strncmp(plistPath, "/var/jb",sizeof("/var/jb")-1) == 0
+	) return false;
+	char plistName[MAXPATHLEN];
+	char* ptr = basename_r(plistPath, plistName);
+	if (ptr == NULL) {
+		NSLog(CFSTR("cfprefsd_hook: basename_r failed: %d (%s)"), errno, strerror(errno));
+		abort();
+	}
+	if (
+		strncmp(plistName, "com.apple.", sizeof("com.apple.")-1) == 0 ||
+		strncmp(plistName, "systemgroup.com.apple.", sizeof("systemgroup.com.apple.")-1) == 0 ||
+		strncmp(plistName, "group.com.apple.", sizeof("group.com.apple.")-1) == 0
+	) return false;
 
-    char* additionalSystemPlistNames[] = {
+	char* additionalSystemPlistNames[] = {
 		".GlobalPreferences.plist",
 		".GlobalPreferences_m.plist",
 		"bluetoothaudiod.plist",
@@ -42,7 +42,7 @@ bool preferencePlistNeedsRedirection(char* plistPath) {
 		"wifid.plist",
 		"dprivacyd.plist",
 		"silhouette.plist",
-        "memoryanalyticsd.plist",
+		"memoryanalyticsd.plist",
 		"nfcd.plist",
 		"kNPProgressTrackerDomain.plist",
 		"siriknowledged.plist",
@@ -54,28 +54,28 @@ bool preferencePlistNeedsRedirection(char* plistPath) {
 		"ptpcamerad.plist",
 		"com.google.gmp.measurement.monitor.plist",
 		"com.google.gmp.measurement.plist",
-        NULL
-    };
+		NULL
+	};
 
-    for (uint8_t i = 0; additionalSystemPlistNames[i] != NULL; i++) {
-        if (strcmp(plistName, additionalSystemPlistNames[i]) == 0) return false;
-    }
-    return true;
+	for (uint8_t i = 0; additionalSystemPlistNames[i] != NULL; i++) {
+		if (strcmp(plistName, additionalSystemPlistNames[i]) == 0) return false;
+	}
+	return true;
 }
 
 bool (*orig_CFPrefsGetPathForTriplet)(CFStringRef, CFStringRef, bool, CFStringRef, char*);
 bool new_CFPrefsGetPathForTriplet(CFStringRef bundleIdentifier, CFStringRef user, bool byHost, CFStringRef path, char *buffer) {
-    bool orig = orig_CFPrefsGetPathForTriplet(bundleIdentifier, user, byHost, path, buffer);
-    if (orig && buffer) {
-        bool needsRedirection = preferencePlistNeedsRedirection(buffer);
-        if (needsRedirection) {
+	bool orig = orig_CFPrefsGetPathForTriplet(bundleIdentifier, user, byHost, path, buffer);
+	if (orig && buffer) {
+		bool needsRedirection = preferencePlistNeedsRedirection(buffer);
+		if (needsRedirection) {
 			NSLog(CFSTR("cfprefsd_hook: Plist redirected to /var/jb: %s"), buffer);
-            char newPath[MAXPATHLEN];
-            snprintf(newPath, MAXPATHLEN, "/var/jb%s", buffer);
-            snprintf(buffer, MAXPATHLEN, "%s", newPath);
+			char newPath[MAXPATHLEN];
+			snprintf(newPath, MAXPATHLEN, "/var/jb%s", buffer);
+			snprintf(buffer, MAXPATHLEN, "%s", newPath);
 		}
-    }
-    return orig;
+	}
+	return orig;
 }
 
 __attribute__((constructor))
