be8650772541c349c64e72be5be2ca11ab9c3e1a
Nick Chan <towinchenmi@gmail.com>
2024-03-23 19:31:14 +0800

    Remove some unneeded declarations

--- src/universalhooks/pineboard.m
+++ src/universalhooks/pineboard.m
@@ -3,42 +3,6 @@
 #include <objc/runtime.h>
 #include <substrate.h>
 
-@interface LSApplicationWorkspace: NSObject
-- (id)allApplications;
-- (void)openApplicationWithBundleID:(NSString *)string;
-- (id)defaultWorkspace;
-@end
-
-@interface NSDistributedNotificationCenter : NSNotificationCenter
-
-+ (id)defaultCenter;
-
-- (void)addObserver:(id)arg1 selector:(SEL)arg2 name:(id)arg3 object:(id)arg4;
-- (void)postNotificationName:(id)arg1 object:(id)arg2 userInfo:(id)arg3;
-
-@end
-
-@interface PBSSystemService : NSObject
-+(id)sharedInstance;
--(void)deactivateScreenSaver;
-@end
-
-@interface PBSPowerManager : NSObject
-
-+(id)sharedInstance;
-+(void)load;
-+(void)setupPowerManagement;
--(void)_performUserEventWakeDevice;
--(void)wakeDeviceWithOptions:(id)arg1;
--(void)setNeedsDisplayWakeOnPowerOn:(BOOL)arg1;
-- (void)sleepDeviceWithOptions:(id)arg1;
--(void)_registerForPowerNotifications;
--(void)_registerForThermalNotifications;
--(void)_enableIdleSleepAndWatchdog;
--(void)_registerForBackBoardNotifications;
--(void)_updateIdleTimer;
-@end
-
 void* PBAppInfo_isEnabled_orig;
 void* PBAppState_isEnabledForApplicationWithIdentifier_orig;
 void* PBSMutableAppState_isEnabled_orig;
