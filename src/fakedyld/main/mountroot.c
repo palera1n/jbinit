da1a38eb7e8ffaafabcfc39b47e00da770c91e48
Nick Chan <towinchenmi@gmail.com>
2024-04-13 15:38:27 +0800

    Update realfs logic

--- src/fakedyld/main/mountroot.c
+++ src/fakedyld/main/mountroot.c
@@ -1,19 +1,6 @@
 #include <fakedyld/fakedyld.h>
 #include <mount_args.h>
 
-const char* find_realfs(void) {
-    struct stat64 st;
-    while (stat64("/dev/disk0s1", &st) == -1) {
-        sleep(1);
-    }
-    if (stat64("/dev/disk2s1", &st) == 0) {
-        return "/dev/disk2s1";
-    } else if (stat64("/dev/disk1s1", &st) == 0) {
-        return "/dev/disk1s1";
-    } else {
-        return "/dec/disk0s1s1";
-    }
-}
 
 /*
  * If the current platform has SSV, palerain_option_ssv
@@ -38,14 +25,16 @@ void mountroot(struct paleinfo* pinfo_p, struct systeminfo* sysinfo_p) {
     int ret;
     char dev_rootdev[32];
     char dev_realfs[32];
+    snprintf(dev_realfs, 32, "/dev/%s", pinfo_p->rootdev);
+    dev_realfs[strlen(dev_realfs)-1] = '1';
+
     if ((pinfo_p->flags & palerain_option_setup_rootful) == 0) {
         snprintf(dev_rootdev, 32, "/dev/%s", pinfo_p->rootdev);
     } else {
-        snprintf(dev_rootdev, 32, "%s", find_realfs());
+        snprintf(dev_rootdev, 32, "%s", dev_realfs);
     }
     LOG("waiting for roots...");
     /* wait for realfs */
-    snprintf(dev_realfs, 32, "%s", find_realfs());
     struct stat64 st;
     while ((ret = stat64(dev_realfs, &st))) {
         if (errno != ENOENT) {
