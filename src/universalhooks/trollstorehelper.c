ee920f1f54b8321e77b2b2e10e8339fc2686ce6d
Nick Chan <towinchenmi@gmail.com>
2024-07-22 00:00:21 +0800

    only skip coretrust bypass if isPlatformVulnerableToExploitType returns 0

--- src/universalhooks/trollstorehelper.c
+++ src/universalhooks/trollstorehelper.c
@@ -1,4 +1,7 @@
 #include <substrate.h>
+#include <limits.h>
+#include <stdint.h>
+
 int apply_coretrust_bypass_hook(__unused char* binaryPath) {
     return 0;
 }
@@ -6,7 +9,11 @@ int apply_coretrust_bypass_hook(__unused char* binaryPath) {
 void trollstorehelperInit(char* executablePath) {
     MSImageRef trollstorehelper = MSGetImageByName(executablePath);
     void* apply_coretrust_bypass = MSFindSymbol(trollstorehelper, "_apply_coretrust_bypass");
-    if (apply_coretrust_bypass) {
-        MSHookFunction(apply_coretrust_bypass, apply_coretrust_bypass_hook, NULL);
+    bool (*isPlatformVulnerableToExploitType)(uint32_t) = MSFindSymbol(trollstorehelper, "_isPlatformVulnerableToExploitType");
+
+    if (isPlatformVulnerableToExploitType && apply_coretrust_bypass) {
+        if (!isPlatformVulnerableToExploitType(UINT32_MAX)) {
+            MSHookFunction(apply_coretrust_bypass, apply_coretrust_bypass_hook, NULL);
+        }
     }
 }
